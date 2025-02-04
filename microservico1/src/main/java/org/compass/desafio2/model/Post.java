c24e01816ff5a8c4007dfd6643dbd820ad23378e
drewneres <hendrewneresfsa@hotmail.com>
2025-02-04 17:40:28 -0300

    feat: Criar classe Post com atributos id, title, body e userId

---
+++ microservico1/src/main/java/org/compass/desafio2/model/Post.java
@@ -0,0 +1,41 @@
+package org.compass.desafio2.model;
+
+public class Post {
+    private Long id;
+    private String title;
+    private String body;
+    private Long userId;
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+}
