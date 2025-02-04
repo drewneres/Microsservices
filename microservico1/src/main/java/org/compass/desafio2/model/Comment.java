82174632c6dee909a973ea168c15e12c227cced7
drewneres <hendrewneresfsa@hotmail.com>
2025-02-04 17:46:38 -0300

    git commit -m "feat: Adiciona a classe Comment para modelar os comentários"

---
+++ microservico1/src/main/java/org/compass/desafio2/model/Comment.java
@@ -0,0 +1,50 @@
+package org.compass.desafio2.model;
+
+public class Comment {
+    private Long id;
+    private String name;
+    private String email;
+    private String body;
+    private Long postId;
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Long getPostId() {
+        return postId;
+    }
+
+    public void setPostId(Long postId) {
+        this.postId = postId;
+    }
+}
