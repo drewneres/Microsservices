2d87fa4f96618d741505a10a85e657db09910830
Matheus Guerra <matheus4xp@gmail.com>
2025-02-04 16:37:41 -0300

    Implementar classe User

---
+++ microservico2/src/main/java/org/compass/desafio2/entidade/Post.java
@@ -0,0 +1,21 @@
+package org.compass.desafio2.entidade;
+
+public class Post {
+    private Long id;
+    private Long userId;
+    private String title;
+    private String body;
+
+    // Getters e Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public Long getUserId() { return userId; }
+    public void setUserId(Long userId) { this.userId = userId; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getBody() { return body; }
+    public void setBody(String body) { this.body = body; }
+}
---
+++ microservico2/src/main/java/org/compass/desafio2/entidade/User.java
@@ -0,0 +1,36 @@
+package org.compass.desafio2.entidade;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String username;
+    private String email;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
