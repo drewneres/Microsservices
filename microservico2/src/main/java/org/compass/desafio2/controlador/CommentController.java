df47dbdf23d85c377b47b456895d096c7053575a
Juan <juancmendonca@gmail.com>
2025-02-04 17:38:42 -0300

    fix: Recursos dos comentários que não subiram no push

--- microservico2/src/main/java/org/compass/desafio2/controlador/CommentController.java
+++ microservico2/src/main/java/org/compass/desafio2/controlador/CommentController.java
@@ -1,4 +1,32 @@
 package org.compass.desafio2.controlador;
 
+import lombok.RequiredArgsConstructor;
+import org.compass.desafio2.entidade.Comment;
+import org.compass.desafio2.servico.CommentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/comments")
+@RequiredArgsConstructor
 public class CommentController {
+
+    private final CommentService commentService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Comment> getById(Integer id) {
+        Comment comment = commentService.getById(id);
+        return ResponseEntity.ok(comment);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Comment>> getAll() {
+        List<Comment> comments = commentService.getAll();
+        return ResponseEntity.ok(comments);
+    }
+
 }
--- microservico2/src/main/java/org/compass/desafio2/servico/CommentService.java
+++ microservico2/src/main/java/org/compass/desafio2/servico/CommentService.java
@@ -1,4 +1,28 @@
 package org.compass.desafio2.servico;
 
+import lombok.RequiredArgsConstructor;
+import org.compass.desafio2.entidade.Comment;
+import org.compass.desafio2.repository.CommentRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
 public class CommentService {
+
+    private final CommentRepository commentRepository;
+
+    @Transactional(readOnly = true)
+    public Comment getById(int id) {
+        return commentRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Comment id not found")
+        );
+    }
+
+    @Transactional(readOnly = true)
+    public List<Comment> getAll() {
+        return commentRepository.findAll();
+    }
 }
