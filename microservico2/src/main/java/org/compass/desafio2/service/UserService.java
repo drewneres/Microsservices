cf9cf73afcdcad06e299b500a49757c4d789bd33
Matheus Guerra <138517790+Guerra457@users.noreply.github.com>
2025-02-05 14:45:25 -0300

    Implementar classe UserService

--- microservico2/src/main/java/org/compass/desafio2/service/UserService.java
+++ microservico2/src/main/java/org/compass/desafio2/service/UserService.java
@@ -1,4 +1,25 @@
 package org.compass.desafio2.service;
 
+import lombok.AllArgsConstructor;
+import org.compass.desafio2.client.JsonPlaceholderClient;
+import org.compass.desafio2.entity.User;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Service
 public class UserService {
+    private final JsonPlaceholderClient jsonPlaceholderClient;
+
+    @Transactional(readOnly = true)
+    public List<User> getAllUsers() {
+        return jsonPlaceholderClient.getAllUsers();
+    }
+
+    @Transactional(readOnly = true)
+    public User getUserById(Long id) {
+        return jsonPlaceholderClient.getUserById(id);
+    }
 }
