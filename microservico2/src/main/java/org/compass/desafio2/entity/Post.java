05bce33ec2c2388e8a04e35c5c66160faa1887d6
palodavis <psilvadias59@gmail.com>
2025-02-05 16:47:12 -0300

    feat: sincronizar dados do post da api e salvar no bd

--- microservico2/src/main/java/org/compass/desafio2/entity/Post.java
+++ microservico2/src/main/java/org/compass/desafio2/entity/Post.java
@@ -1,21 +1,19 @@
 package org.compass.desafio2.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@Table(name = "tb_post")
 public class Post {
+    @Id
     private Long id;
     private Long userId;
     private String title;
     private String body;
 
-    // Getters e Setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public Long getUserId() { return userId; }
-    public void setUserId(Long userId) { this.userId = userId; }
-
-    public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
-
-    public String getBody() { return body; }
-    public void setBody(String body) { this.body = body; }
+    public Post() {}
 }
