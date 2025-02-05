04d38e281a1765d88831ca945fbb0b04ae1ce538
drewneres <hendrewneresfsa@hotmail.com>
2025-02-05 16:13:36 -0300

    Feat: Atualizar controller para expor endpoints de criação, atualização e exclusão

--- microservico1/src/main/java/org/compass/desafio2/client/MicrosservicoBClient.java
+++ microservico1/src/main/java/org/compass/desafio2/client/MicrosservicoBClient.java
@@ -3,8 +3,13 @@ package org.compass.desafio2.client;
 import org.compass.desafio2.model.Post;
 import org.compass.desafio2.model.Comment;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -19,4 +24,20 @@ public interface MicrosservicoBClient {
 
     @GetMapping("/api/posts/{id}/comments")
     List<Comment> getCommentsByPostId(@PathVariable Long id);
+
+        // POST
+    @PostMapping("/api/posts")
+    Post createPost(@RequestBody Post post);
+
+    // PUT (atualização completa)
+    @PutMapping("/api/posts/{id}")
+    Post updatePost(@PathVariable Long id, @RequestBody Post post);
+
+    // PATCH (atualização parcial)
+    @PatchMapping("/api/posts/{id}")
+    Post patchPost(@PathVariable Long id, @RequestBody Map<String, Object> updates);
+
+    // DELETE
+    @DeleteMapping("/api/posts/{id}")
+    void deletePost(@PathVariable Long id);
 }
