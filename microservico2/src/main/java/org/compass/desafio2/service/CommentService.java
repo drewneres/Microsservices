3b940cdd3e79cb4008f8a00e45a037b694ab4fd2
Juan <juancmendonca@gmail.com>
2025-02-06 15:47:35 -0300

    feat: Finalizado as operações CRUD dos Comments

--- microservico2/src/main/java/org/compass/desafio2/service/CommentService.java
+++ microservico2/src/main/java/org/compass/desafio2/service/CommentService.java
@@ -1,8 +1,11 @@
 package org.compass.desafio2.service;
 
 import lombok.RequiredArgsConstructor;
+import org.compass.desafio2.client.JsonPlaceholderClient;
 import org.compass.desafio2.entity.Comment;
+import org.compass.desafio2.entity.Post;
 import org.compass.desafio2.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,6 +16,14 @@ import java.util.List;
 public class CommentService {
 
     private final CommentRepository commentRepository;
+
+    private final JsonPlaceholderClient jsonPlaceholderClient;
+
+    @Transactional
+    public void syncData() {
+        List<Comment> comments = jsonPlaceholderClient.getAllComments();
+        commentRepository.saveAll(comments);
+    }
 
     @Transactional(readOnly = true)
     public Comment getById(Long id) {
@@ -35,12 +46,14 @@ public class CommentService {
         }
     }
 
-    //Passar o Dto para atualizar só o body e name
-//    public Comment update(Comment comment) {
-//        return commentRepository.save(comment);
-//    }
-//
-//    public void deleteById(int id) {
-//        commentRepository.deleteById(id);
-//    }
+    public void deleteById(Long id) {
+        commentRepository.deleteById(id);
+    }
+
+    public Comment updateComment(Long id, String name, String body) {
+        getById(id).setName(name);
+        getById(id).setBody(body);
+        return commentRepository.save(getById(id));
+    }
+
 }
